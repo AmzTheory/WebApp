00927b65cfd3714f277ad899cfc61dbf78b94d40
Ahmed <Ahmedamz@outlook.com>
2018-03-31 19:24:05 +0000

    validation added to contact orm

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,7 @@
 class Contact <ActiveRecord::Base
     
+    #validate that name , email and comments is entered
+    validates :name, presence: true
+    validates :email ,presence: true
+    validates :comments ,presence: true
 end
